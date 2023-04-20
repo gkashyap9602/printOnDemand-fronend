ac3101632cc568adafb8d0bee8f1da69b68bc2e3
portal-mwwondemand <108809295+portal-mwwondemand@users.noreply.github.com>
2023-04-20 17:52:35 +0530

    Update handler.js

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -6,9 +6,5 @@
  * @param {*} res
  */
 export default function handler(req, res) {
-  res
-    .status(200)
-    .json(
-      '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6yJI/o2VN7Wjv5zKYIBjDW0+6MN4pu1DIYXeWYdhNzf4kKZqaM4DYDqGeBEW544OJ+3sQd92pyYbrW1YDd4kv2jA5kOTeWDuiaY1hisXPc3Kh1mLkFryDyURMkV+orp8MTdeulgE1QNeQnxutn47OSfZgB1ycWNN2n+MJmApdA024rhUiCj6MxOJICxkW4E9rzPgOhZwjAf6isUBTSPZwRRkt6fmsJ5hbzhAAIKQhNyEwYyYxwfulRrbNvDWifB6NU4UZKoUanUElRW2WGxh161QADoqI8gTNvP6NUoyedMRyC0GjpD6kPG1hHEfI9LKt8E3fVaKrapxXuchHucH9QIDAQAB-----END PUBLIC KEY-----'
-    )
+  res.status(200).json(process.env.PUBLIC_KEY)
 }
